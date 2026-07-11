b56724256a34290340bfbd92d92cbbd7aa86fd6c
asemanha-creator <asemanha@gmail.com>
2026-07-11 08:32:42 +0330

    Update get.js

--- pages/api/get.js
+++ pages/api/get.js
@@ -1,4 +1,4 @@
-تimport { kv } from "@vercel/kv";
+import { kv } from "@vercel/kv";
 
 export default async function handler(req, res) {
   if (req.method !== "GET") return res.status(405).json({ error: "method not allowed" });
