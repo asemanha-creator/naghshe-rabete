4312ad6f4d6f408e36a690b09cebd002d1a656d5
asemanha-creator <asemanha@gmail.com>
2026-08-15 16:27:03 +0330

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,9 @@ export default function Document() {
     <Html lang="fa" dir="rtl">
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <meta name="theme-color" content="#2B6777" />
       </Head>
       <body>
         <Main />
