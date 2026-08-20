290b6cc3b440104c700fcf54f489dbdd8bc0c415
asemanha-creator <asemanha@gmail.com>
2026-08-20 09:17:56 +0330

    Update session-library.js

--- pages/api/session-library.js
+++ pages/api/session-library.js
@@ -38,7 +38,7 @@ export default async function handler(req, res) {
       const sess = getSessionContent(pkgKey, num, weakestDomain || null, "excellent");
       if (searchQuery && !sessionMatchesSearch(sess, searchQuery)) continue;
       const sid = `${pkgKey}-${num}`;
-      const unlocked = num === 1 ? true : (isAdmin || unlockedSessions.includes(sid));
+      const unlocked = num === 1 || pkgKey === "distrust" ? true : (isAdmin || unlockedSessions.includes(sid));
       const item = { num, title: sess.title, approach: sess.approach, unlocked };
       results.push(item);
     }
